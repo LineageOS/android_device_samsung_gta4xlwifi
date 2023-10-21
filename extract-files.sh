7b0581956d743fbaa071d836ab2900aea4378375
Tim Zimmermann <tim@linux4.de>
2023-10-21 14:40:21 +0200

    gta4xlwifi: Build audio HAL from source
    Change-Id: I17978d0e0b9ed71ff3a12e84d92d25a1396012af

--- extract-files.sh
+++ extract-files.sh
@@ -6,14 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib/hw/audio.primary.exynos9611.so)
-            "${PATCHELF}" --remove-needed libaudio_soundtrigger.so "${2}"
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
