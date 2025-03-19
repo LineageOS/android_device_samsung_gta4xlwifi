ab9fc5d151a5d9e985ab456a31fd57bccb6c54f8
Tim Zimmermann <tim@linux4.de>
2025-03-19 19:58:23 +0100

    gta4xlwifi: Switch to py extract utils
    Change-Id: I519d5e02f1fef816ae4615a1923424ca8ceb9237

---
+++ extract-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/samsung_slsi-linaro/exynos',
+    'hardware/samsung_slsi-linaro/graphics',
+    'vendor/samsung/gta4xl-common',
+]
+
+module = ExtractUtilsModule(
+    'gta4xlwifi',
+    'samsung',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'gta4xl-common', module.vendor
+    )
+    utils.run()
